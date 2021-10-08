8f8fbd502559d1baf7527bf3991507bb81fa6aea
GregorVolcanjk <65243657+GregorVolcanjk@users.noreply.github.com>
2021-10-08 16:19:50 +0200

    Finished step 3

--- index.js
+++ index.js
@@ -1,112 +1,104 @@
 const express = require('express');
-const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(
-	morgan((tokens, req, res) => {
-		return [
-			tokens.method(req, res),
-			tokens.url(req, res),
-			tokens.status(req, res),
-			tokens.res(req, res, 'content-length'),
-			'-',
-			tokens['response-time'](req, res),
-			'ms',
-			tokens.data(req, res),
-		].join(' ');
-	})
-);
 
-morgan.token('data', (request) => {
-	return request.method === 'POST' ? JSON.stringify(request.body) : null;
-});
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method);
+	console.log('Path:  ', request.path);
+	console.log('Body:  ', request.body);
+	console.log('---');
+	next();
+};
 
-let persons = [
+app.use(requestLogger);
+
+let notes = [
 	{
 		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
+		content: 'HTML is easy',
+		date: '2020-01-10T17:30:31.098Z',
+		important: true,
 	},
 	{
 		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
+		content: 'Browser can execute only Javascript',
+		date: '2020-01-10T18:39:34.091Z',
+		important: false,
 	},
 	{
 		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
+		content: 'GET and POST are the most important methods of HTTP protocol',
+		date: '2020-01-10T19:20:14.298Z',
+		important: true,
 	},
 ];
 
-app.get('/api/persons/:id', (request, response, next) => {
-	const id = Number(request.params.id);
-	const person = persons.find((person) => person.id === id);
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>');
+});
 
-	if (person) {
-		response.json(person);
+app.get('/api/notes', (req, res) => {
+	res.json(notes);
+});
+
+const generateId = () => {
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+	return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+	const body = request.body;
+
+	if (!body.content) {
+		return response.status(400).json({
+			error: 'content missing',
+		});
+	}
+
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+		id: generateId(),
+	};
+
+	notes = notes.concat(note);
+
+	response.json(note);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((note) => note.id === id);
+
+	if (note) {
+		response.json(note);
 	} else {
 		response.status(404).end();
 	}
 });
 
-app.get('/api/persons', (request, response) => {
-	response.json(persons);
-});
-
-app.get('/info', (request, response) => {
-	response.send(
-		`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
-	);
-});
-
-const generateId = () => {
-	return Math.round(Math.random() * 100);
-};
-
-app.post('/api/persons', (request, response, next) => {
-	const body = request.body;
-
-	if (!body.number) {
-		return response.status(400).json({ error: 'number missing' });
-	}
-
-	if (!body.name) {
-		return response.status(400).json({ error: 'name missing' });
-	}
-
-	const people = persons.filter((person) => person.name === body.name);
-
-	if (people.length > 0) {
-		return response.status(400).json({
-			error: people,
-		});
-	}
-
-	const person = {
-		name: body.name,
-		number: body.number,
-		id: generateId(),
-	};
-
-	persons = persons.concat(person);
-
-	response.json(person);
-});
-
-app.delete('/api/persons/:id', (request, response, next) => {
+app.delete('/api/notes/:id', (request, response) => {
 	const id = Number(request.params.id);
-	const people = persons.filter((person) => person.id !== id);
+	notes = notes.filter((note) => note.id !== id);
 
 	response.status(204).end();
 });
+
+app.put('/api/notes/:id', (request,response) => {
+	const id = Number(request.params.id)
+	
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
